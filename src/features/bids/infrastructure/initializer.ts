ca6bc0dbf08e47d441d5a846401af85717dc26a8
alexsanya <alexsanyakoval@gmail.com>
2024-12-29 23:58:24 -0500

    add test for bid routes

--- src/features/bids/infrastructure/initializer.ts
+++ src/features/bids/infrastructure/initializer.ts
@@ -1,7 +1,34 @@
 import { type RedisClientType, type RediSearchSchema, SchemaFieldTypes } from 'redis';
+import { DOMAIN_SEPARATORS, SEPOLIA_ID } from '../../../core';
+import { CreateListingDto } from '../../listings';
 
 export const BID_INDEX_KEY = 'bid:index';
 export const BID_KEY_PREFIX = 'bid:';
+
+const BID_ONE = {
+	bidder: '0xE98D94496aB9084f597a69978b593EBf83147335',
+	listing: {
+		owner: '0x3897326cEda92B3da2c27a224D6fDCFefCaCf57A',
+		chainId: '11155111',
+		minPriceCents: '150000',
+		nftContract: '0xf44b599a0aB6b8cb14E992994BEC0dc59dF883B2',
+		tokenId: '1',
+		nonce: '0',
+		signature: {
+			v: 28,
+			r: '0x5ef4620f4b296763ff15209456d75e868f149a8d1c6821f1ff11fab70bca0ee0',
+			s: '0x337ddcb26ea919a2bf5ad6e1d49bd6951a27d1d2e940a5543a70eabc5dbe237e'
+		}
+	},
+	tokenAddress: '0xc29f6F8D639eF187DcFEfeFBaD989cF2C941a23A',
+	validUntil: '1735504160',
+	value: '250',
+	signature: {
+		v: 28,
+		r: '0x1469ac6f9636c24d2d8c3fb2cbef73708876e15f23f23b1d33863939c905a21c',
+		s: '0x7d9a7ea039465c928311bcb737b23153232028038beadba2a667aa720f17602b'
+	}
+};
 
 export class BidDataInitializer {
 	public static async initBidsIndex(client: RedisClientType): Promise<void> {
@@ -32,53 +59,10 @@ export class BidDataInitializer {
 	}
 
 	public static async populateBids(client: RedisClientType): Promise<void> {
-		await client.json.set('bid:1', '.', {
-			bidder: '0x07152bfde079b5319e5308c43fb1dbc9c76cb4f9',
-			listing: {
-				owner: '0x07152bfde079b5319e5308c43fb1dbc9c76cb4f9',
-				chainId: '11155111',
-				minPriceCents: '150000',
-				nftContract: '0x07152bfde079b5319e5308c43fb1dbc9c76cb4f9',
-				tokenId: '72997',
-				nonce: '0',
-				signature: {
-					v: 5,
-					r: '0x05416460deb76d57af601be17e777b93592d8d4d4a4096c57876a91c84f4a712',
-					s: '0x05416460deb76d57af601be17e777b93592d8d4d4a4096c57876a91c84f4a712'
-				}
-			},
-			tokenAddress: '0x07152bfde079b5319e5308c43fb1dbc9c76cb4f9',
-			validUntil: '1735255023397',
-			value: '100500',
-			signature: {
-				v: 5,
-				r: '0x05416460deb76d57af601be17e777b93592d8d4d4a4096c57876a91c84f4a712',
-				s: '0x05416460deb76d57af601be17e777b93592d8d4d4a4096c57876a91c84f4a712'
-			}
-		});
-		await client.json.set('bid:2', '.', {
-			bidder: '0x07152bfde079b5319e5308c43fb1dbc9c76cb4f9',
-			listing: {
-				owner: '0x5d2fcc71dFf7182bf49927a2ed3C8FcE0De87723',
-				chainId: '11155111',
-				minPriceCents: '150000',
-				nftContract: '0x07152bfde079b5319e5308c43fb1dbc9c76cb4f9',
-				tokenId: '72997',
-				nonce: '0',
-				signature: {
-					v: 5,
-					r: '0x05416460deb76d57af601be17e777b93592d8d4d4a4096c57876a91c84f4a712',
-					s: '0x05416460deb76d57af601be17e777b93592d8d4d4a4096c57876a91c84f4a712'
-				}
-			},
-			tokenAddress: '0x07152bfde079b5319e5308c43fb1dbc9c76cb4f9',
-			validUntil: '1735255023397',
-			value: '100500',
-			signature: {
-				v: 5,
-				r: '0x05416460deb76d57af601be17e777b93592d8d4d4a4096c57876a91c84f4a712',
-				s: '0x05416460deb76d57af601be17e777b93592d8d4d4a4096c57876a91c84f4a712'
-			}
-		});
+		await client.json.set(
+			`bid:${CreateListingDto.create(BID_ONE.listing).hash(DOMAIN_SEPARATORS[SEPOLIA_ID])}`,
+			'.',
+			BID_ONE
+		);
 	}
 }
--- src/routes.test.ts
+++ src/routes.test.ts
@@ -3,6 +3,7 @@ import { envs, HttpCode, SuccessResponse } from './core';
 import { testServer } from './testServer';
 import { PaginationResponseEntity } from './features/shared';
 import { ListingEntity } from './features/listings';
+import { BidEntity } from './features/bids';
 
 const NEW_LISTING = {
 	owner: '0x3897326cEda92B3da2c27a224D6fDCFefCaCf57A',
@@ -18,45 +19,110 @@ const NEW_LISTING = {
 	}
 };
 
-describe('test listing routes', () => {
-	const url = `${envs.API_PREFIX}/listings`;
+const NEW_BID = {
+	bidder: '0xE98D94496aB9084f597a69978b593EBf83147335',
+	listing: {
+		owner: '0x3897326cEda92B3da2c27a224D6fDCFefCaCf57A',
+		chainId: '11155111',
+		minPriceCents: '150000',
+		nftContract: '0xf44b599a0aB6b8cb14E992994BEC0dc59dF883B2',
+		tokenId: '1',
+		nonce: '0',
+		signature: {
+			v: 28,
+			r: '0x5ef4620f4b296763ff15209456d75e868f149a8d1c6821f1ff11fab70bca0ee0',
+			s: '0x337ddcb26ea919a2bf5ad6e1d49bd6951a27d1d2e940a5543a70eabc5dbe237e'
+		}
+	},
+	tokenAddress: '0xc29f6F8D639eF187DcFEfeFBaD989cF2C941a23A',
+	validUntil: '1735504160',
+	value: '250',
+	signature: {
+		v: 28,
+		r: '0x1469ac6f9636c24d2d8c3fb2cbef73708876e15f23f23b1d33863939c905a21c',
+		s: '0x7d9a7ea039465c928311bcb737b23153232028038beadba2a667aa720f17602b'
+	}
+};
 
+describe('test routes', () => {
 	beforeAll(async () => {
 		await testServer.start();
 	});
 
-	test('should return listings /listings', async () => {
-		await request(testServer.app)
-			.get(url)
-			.expect(HttpCode.OK)
-			.expect('Content-Type', /json/)
-			.then(({ body }: { body: SuccessResponse<PaginationResponseEntity<ListingEntity[]>> }) => {
-				expect(body).toMatchSnapshot();
-				expect(body.data?.results.length).toBe(1);
-			});
+	describe('test bid routes', () => {
+		const url = `${envs.API_PREFIX}/bids`;
+
+		test('should return bids /bids', async () => {
+			await request(testServer.app)
+				.get(url)
+				.expect(HttpCode.OK)
+				.expect('Content-Type', /json/)
+				.then(({ body }: { body: SuccessResponse<PaginationResponseEntity<ListingEntity[]>> }) => {
+					expect(body).toMatchSnapshot();
+					expect(body.data?.results.length).toBe(1);
+				});
+		});
+
+		test('should fail creating new bid if signature is invalid', async () => {
+			await request(testServer.app)
+				.post(url)
+				.send({
+					...NEW_BID,
+					signature: {
+						...NEW_BID.signature,
+						v: 27
+					}
+				})
+				.expect(HttpCode.BAD_REQUEST);
+		});
+
+		test('should create new bid /bids', async () => {
+			await request(testServer.app)
+				.post(url)
+				.send(NEW_BID)
+				.expect(HttpCode.CREATED)
+				.expect('Content-Type', /json/)
+				.then(({ body }: { body: SuccessResponse<BidEntity> }) => {
+					expect(body).toMatchSnapshot();
+				});
+		});
 	});
 
-	test('should fail creating new listing if signature is invalid', async () => {
-		await request(testServer.app)
-			.post(url)
-			.send({
-				...NEW_LISTING,
-				signature: {
-					...NEW_LISTING.signature,
-					v: 27
-				}
-			})
-			.expect(HttpCode.BAD_REQUEST);
-	});
+	describe('listing routes', () => {
+		const url = `${envs.API_PREFIX}/listings`;
+		test('should return listings /listings', async () => {
+			await request(testServer.app)
+				.get(url)
+				.expect(HttpCode.OK)
+				.expect('Content-Type', /json/)
+				.then(({ body }: { body: SuccessResponse<PaginationResponseEntity<ListingEntity[]>> }) => {
+					expect(body).toMatchSnapshot();
+					expect(body.data?.results.length).toBe(1);
+				});
+		});
 
-	test('should create new listing /listings', async () => {
-		await request(testServer.app)
-			.post(url)
-			.send(NEW_LISTING)
-			.expect(HttpCode.CREATED)
-			.expect('Content-Type', /json/)
-			.then(({ body }: { body: SuccessResponse<ListingEntity> }) => {
-				expect(body).toMatchSnapshot();
-			});
+		test('should fail creating new listing if signature is invalid', async () => {
+			await request(testServer.app)
+				.post(url)
+				.send({
+					...NEW_LISTING,
+					signature: {
+						...NEW_LISTING.signature,
+						v: 27
+					}
+				})
+				.expect(HttpCode.BAD_REQUEST);
+		});
+
+		test('should create new listing /listings', async () => {
+			await request(testServer.app)
+				.post(url)
+				.send(NEW_LISTING)
+				.expect(HttpCode.CREATED)
+				.expect('Content-Type', /json/)
+				.then(({ body }: { body: SuccessResponse<ListingEntity> }) => {
+					expect(body).toMatchSnapshot();
+				});
+		});
 	});
 });
